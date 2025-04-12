2607d05d7134538db575410ce2ccade085bc5cd7
Artur Henrique <hharturcarmello@gmail.com>
2025-04-12 19:42:14 -0300

    Update scriptHub.lua

--- scriptHub.lua
+++ scriptHub.lua
@@ -1,2 +1,13 @@
 print("Hello from github")
-print(scriptHub.API_KEY)
+local url = "http://localhost:3000".."/".. scriptHub.API_KEY
+
+local sucess, response = pcall(function()
+	return Http:GetAsync(url)
+end)
+local _, response = pcall(function()
+	return Http:JSONDecode(response)
+end)
+
+if response and response.status == 200 then
+  print("Natan buti")
+end
